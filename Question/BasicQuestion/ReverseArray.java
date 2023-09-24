68e5f16214ccd8cdb0ef8008bd68a5fa7276d7ac
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2023-09-24 08:49:36 +0530

    2d array completed

---
+++ Question/BasicQuestion/ReverseArray.java
@@ -0,0 +1,29 @@
+package BasicQuestion;
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		reverse(arr);
+
+		// Print the reversed array
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+
+	public static void reverse(int[] arr) {
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start < end) {
+			// Swap the elements at start and end positions
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+
+			// Move the pointers towards the center of the array
+			start++;
+			end--;
+		}
+	}
+}
