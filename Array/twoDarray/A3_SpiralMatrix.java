fe1e21fed799f43092ceb8417f16f092bd6f6985
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2023-10-05 07:11:00 +0530

    Update A3_SpiralMatrix.java

--- Array/twoDarray/A3_SpiralMatrix.java
+++ Array/twoDarray/A3_SpiralMatrix.java
@@ -1,13 +1,50 @@
 package twoDarray;
 
 public class A3_SpiralMatrix {
-	public static void printSpiral(int m[][]) {
+	
+    public static void printSpiralMatrix(int[][] matrix) {
+        int startRow = 0, endRow = matrix.length - 1, startColumn = 0, endColumn = matrix[0].length - 1;
 
-	}
+        while (startRow <= endRow && startColumn <= endColumn) {
+            // Print top row
+            for (int i = startColumn; i <= endColumn; i++) {
+                System.out.print(matrix[startRow][i] + " ");
+            }
+            startRow++;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int m[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-	}
+            // Print right column
+            for (int i = startRow; i <= endRow; i++) {
+                System.out.print(matrix[i][endColumn] + " ");
+            }
+            endColumn--;
+
+            // Print bottom row (if exists)
+            if (startRow <= endRow) {
+                for (int i = endColumn; i >= startColumn; i--) {
+                    System.out.print(matrix[endRow][i] + " ");
+                }
+                endRow--;
+            }
+
+            // Print left column (if exists)
+            if (startColumn <= endColumn) {
+                for (int i = endRow; i >= startRow; i--) {
+                    System.out.print(matrix[i][startColumn] + " ");
+                }
+                startColumn++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        printSpiralMatrix(matrix);
+    }
+
 
 }
